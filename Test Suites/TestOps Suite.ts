2698249ecb45054990416160116e09537c429ed7
Padala.P.Goud <Padala.P.Goud@LP-HYD-0X105111.IN.COFORGETECH.COM>
2025-11-19 22:59:26 +0530

    second commit

---
+++ Test Suites/TestOps Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestOps Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3f57d506-5bf2-4b25-bfff-65fbd7ff458e</testSuiteGuid>
+   <testCaseLink>
+      <guid>a1446083-2aae-4416-b93b-5c1c839668bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOps integration/TestOps Testing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
